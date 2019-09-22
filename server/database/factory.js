f933e897f7456a3645bc08fcf3f20c2a9da37032
JosMMV <jsmiguel1702@gmail.com>
2019-09-21 21:25:35 -0400

    semillas de user, commerce, product y subsidiary

--- server/database/factory.js
+++ server/database/factory.js
@@ -12,20 +12,41 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+const Factory = use('Factory')
+const Hash = use('Hash')
 
 Factory.blueprint('App/Models/User', async faker => {
   return {
-    username: faker.username(),
+    username: faker.email(),
     password: await Hash.make(faker.password()),
     created_at: faker.date()
   }
 })
 
+Factory.blueprint('App/Models/Commerce', async faker => {
+  return {
+    user_id: async () => {
+      return (await Factory.model('App/Models/User').create()).id
+    },
+    rif: faker.ssn({ dashes: false }),
+    nombre: faker.company(),
+  }
+});
+
 Factory.blueprint('App/Models/Product', faker => {
   return {
-    nombre: faker.sentence({words: 2}),
-    precio: faker.floating({min: 24000, max: 1000000}),
-    volumen: faker.floating({min: 3, max: 50 })
+    nombre: faker.word(),
+    precio: faker.floating({min: 15500, max: 1000000 }),
+    volumen: faker.floating({min: 10, max: 150 })
+  } 
+})
+
+Factory.blueprint('App/Models/Subsidiary', faker => {
+  return {
+    nombre: faker.animal(),
+    estado: faker.state(),
+    ciudad: faker.city(),
+    parroquia: faker.province(),
+    distanciaDesdeCaracas: faker.natural({min: 10, max: 800})
   } 
 })
--- server/database/seeds/DatabaseSeeder.js
+++ server/database/seeds/DatabaseSeeder.js
@@ -15,6 +15,11 @@ const Factory = use('Factory')
 
 class DatabaseSeeder {
   async run () {
+    const commerceArray = await Factory.model('App/Models/Commerce').createMany(5)
+
+    const productArray = await Factory.model('App/Models/Product').createMany(15)
+
+    const subsidiaryArray = await Factory.model('App/Models/Subsidiary').createMany(24)
   }
 }
 
