e08e1a8e410b59bff79cdc43ca0faa6343639665
JosMMV <jsmiguel1702@gmail.com>
2019-09-22 11:59:25 -0400

    se quitaron los timestamps del modelo Product

--- server/app/Models/Product.js
+++ server/app/Models/Product.js
@@ -4,6 +4,14 @@
 const Model = use('Model')
 
 class Product extends Model {
+  static get createdAtColumn () {
+    return null;
+  }
+
+  static get updatedAtColumn() {
+    return null;
+  }
+
   orders () {
     return this.belongsToMany('App/Models/Order')
   }
